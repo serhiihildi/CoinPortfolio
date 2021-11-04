5b507e0b980989bac894141330e711c05f7e9163
sergeygildi <sergeygildi@gmail.com>
2021-11-05 00:29:09 +0200

    Implemented the main functionality of the application

--- src/main/java/com/petProject/portfolio/PortfolioApplication.java
+++ src/main/java/com/petProject/portfolio/PortfolioApplication.java
@@ -5,25 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PortfolioApplication {
 
-//	public static void main(String[] args) {
-//		SpringApplication.run(PortfolioApplication.class, args);
-//		Coin model = retrieveCoinFromDatabase();
-//		CoinView view = new CoinView();
-//
-//		CoinController controller = new CoinController(model, view);
-
-//		controller.updateView();
-//
-//		controller.setCoin("BTCADA");
-//		controller.updateView();
-//	}
-//
-//	private static Coin retrieveCoinFromDatabase() {
-//		Coin coin = new Coin();
-//		coin.setCoinName("BTCUSDT");
-//		coin.setCoinCurrentPrice(60000.10f);
-//		return coin;
-//
-//	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DataApplication.class, args);
+    }
 }
